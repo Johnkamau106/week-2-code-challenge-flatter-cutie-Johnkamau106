d78cb3083209529a33037673bd9eee11b3f0320a
johnkamau106 <john.kamau7@student.moringaschool.com>
2025-03-24 17:18:15 +0300

    changed css on how the image appears and also how characters appear in js

--- src/index.js
+++ src/index.js
@@ -8,4 +8,26 @@ document.addEventListener('DOMContentLoaded', () => {
     .then(characters => {
       characters.forEach(character => {
        
-   
+        const characterSpan = document.createElement('span');
+        characterSpan.textContent = character.name;
+
+        
+        characterSpan.addEventListener('click', () => {
+          displayCharacterDetails(character);
+        });
+
+        
+        characterBar.appendChild(characterSpan);
+      });
+    })
+    .catch(error => console.error('Error fetching characters:', error));
+
+  
+  function displayCharacterDetails(character) {
+    detailedInfo.innerHTML = `
+      <h2>${character.name}</h2>
+      <img src="${character.image}" alt="${character.name}">
+      <p>Votes: <span id="vote-count">${character.votes}</span></p>
+    `;
+  }
+});
